79713c288487c01a9628671b5475218143c15f15
HilbertCurve <80780490+HilbertCurve@users.noreply.github.com>
2021-10-28 21:52:13 -0400

    started rend printing

--- include/Utils.h
+++ include/Utils.h
@@ -1,9 +1,14 @@
 /* date = October 17th 2021 5:25 pm */
 
-#ifndef _PONTILUS_UTILS_H
-#define _PONTILUS_UTILS_H
+#pragma once
+
+//#ifndef _PONTILUS_UTILS_H
+//#define _PONTILUS_UTILS_H
 
 #include <stdint.h>
+#include "Rend.h"
+#include "GameObject.h"
+#include "Renderer.h"
 
 namespace Pontilus
 {
@@ -12,14 +17,16 @@ namespace Pontilus
     /**
      * Use this struct if you want to return a pair of useful data in a function.
      */
-    //template<typename T.
+    template<typename T, typename U>
     struct BiReturn
     {
-        byte first;
-        int second;
+        T first;
+        U second;
     };
 
-    void loadFile(const char *filepath, char *data);
+    // by way of the GCC folks and whatever goes on there,
+    void printRend(Graphics::Rend &r); // THIS IS A VARIABLE,
+    void loadFile(const char *filepath, char *data); // WHILE THIS IS NOT?
 }
 
-#endif // _PONTILUS_UTILS_H
+//#endif // _PONTILUS_UTILS_H
